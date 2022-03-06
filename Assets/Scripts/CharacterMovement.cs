38f253e1ec79cc2c58e2445a2e571eef589ac265
Dragunova Tatiana <t.dragunova@kevurugames.com>
2022-03-06 16:52:10 +0300

    Character movement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent characterAgent;
+    [SerializeField] private Camera mainCamera;
+    
+    private void Awake()
+    {
+        SetNavMeshAgent();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 destination = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            MoveCharacterToDestination(destination);
+        }
+    }
+
+    private void SetNavMeshAgent()
+    {
+        characterAgent.updateRotation = false;
+        characterAgent.updateUpAxis = false;
+    }
+
+    private void MoveCharacterToDestination(Vector3 destination)
+    {
+        characterAgent.SetDestination(destination);
+    }
+}
